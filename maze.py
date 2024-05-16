c670d21e528c973acb905d549b123fbbba8de19d
Pietot <102545552+Pietot@users.noreply.github.com>
2024-05-16 13:36:54 +0200

    Updated module docstring

--- maze.py
+++ maze.py
@@ -1,5 +1,4 @@
-""" A program capable of creating mazes with many different algorithms
-and solving them with different pathfinders """
+""" A program capable of creating mazes with many different algorithms """
 
 
 # By Pietot
