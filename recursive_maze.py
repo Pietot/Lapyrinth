480bceb5393dfa574ee51462b3c2a02f29991e12
Baptiste <baptiste.blasquez@gmail.com>
2024-06-10 14:42:25 +0200

    Minor changes

--- recursive_maze.py
+++ recursive_maze.py
@@ -181,23 +181,31 @@ class RecursiveMaze:
         start = start if start else get_random_cell(
             (self.maze.shape[0], self.maze.shape[1]))
 
-        def hunt() -> tuple[int, int] | None:
+        def hunt() -> None:
             unvisited_cells = np.argwhere(self.maze > 2)
+
             if not unvisited_cells.size:
                 return None
+
             unvisited_cells = unvisited_cells.tolist()
+
             for cell_index in unvisited_cells:
+                
                 neighbor, direction = get_connection(
                     self, (cell_index[0], cell_index[1]))
+                
                 if neighbor == (0, 0):
                     continue
+                
                 self.maze[neighbor] = 2
                 wall_coordinates = (
                     neighbor[0] - direction[0] // 2,
                     neighbor[1] - direction[1] // 2,
                 )
+
                 self.maze[wall_coordinates] = 2
-                return self.recursive_hunt_and_kill((cell_index[0], cell_index[1]))
+                self.recursive_hunt_and_kill((cell_index[0], cell_index[1]))
+
             return None
 
         def kill(cell: tuple[int, int]) -> None:
