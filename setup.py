7b7aace5afa83c8d156ab858cc56803e75a40693
Pietot <102545552+Pietot@users.noreply.github.com>
2024-06-28 23:25:30 +0200

    Updated file

--- setup.py
+++ setup.py
@@ -10,10 +10,12 @@ setup(
     ],
     author="Piétôt",
     author_email="baptiste.blasquez@gmail.com",
+    maintainer="Piétôt",
     description="A program capable of creating mazes with many different algorithms and solving them with different pathfinders.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Pietot/Lapyrinth",
+    keywords=["maze", "pathfinding", "algorithm", "labyrinth", "pathfinder", "maze generator"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
