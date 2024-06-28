81ce6260a19c1b546a78a2663aae04f082a90001
Pietot <102545552+Pietot@users.noreply.github.com>
2024-06-28 23:32:45 +0200

    Updated file

--- setup.py
+++ setup.py
@@ -1,18 +1,21 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+    
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+    
 setup(
     name="Lapyrinth",
     version="1.13",
     packages=find_packages(),
-    install_requires=[
-        "numpy==2.0.0",
-        "Pillow==10.2.0",
-    ],
+    install_requires=required,
     author="Piétôt",
     author_email="baptiste.blasquez@gmail.com",
     maintainer="Piétôt",
     description="A program capable of creating mazes with many different algorithms and solving them with different pathfinders.",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Pietot/Lapyrinth",
     keywords=["maze", "pathfinding", "algorithm", "labyrinth", "pathfinder", "maze generator"],
