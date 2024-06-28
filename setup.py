cabe4a8eb10c06280923f9446a9b8bff4ea355b6
Pietot <102545552+Pietot@users.noreply.github.com>
2024-06-28 23:15:48 +0200

    Added file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Lapyrinth",
+    version="1.13",
+    packages=find_packages(),
+    install_requires=[
+        "numpy==2.0.0",
+        "Pillow==10.2.0",
+    ],
+    author="Piétôt",
+    author_email="baptiste.blasquez@gmail.com",
+    description="A program capable of creating mazes with many different algorithms and solving them with different pathfinders.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Pietot/Lapyrinth",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
