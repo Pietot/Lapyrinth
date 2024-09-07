a4c0244d1789a6044f5e1da4be40f1d39909ba8e
Pietot <102545552+Pietot@users.noreply.github.com>
2024-09-08 01:53:45 +0200

    Updated version from 1.13 to 1.14

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
-with open('requirements.txt') as f:
+with open("requirements.txt") as f:
     required = f.read().splitlines()
-    
+
 long_description = "This Mazer Maker Solver made entirely in Python is a program where you can generate maze with many different algorithm and solving them with different pathfinders. Nothing more, nothing less. More info at https://github.com/Pietot/Lapyrinth"
-    
+
 setup(
     name="lapyrinth",
-    version="1.13",
+    version="1.14",
     packages=find_packages(),
     install_requires=required,
     author="Piétôt",
