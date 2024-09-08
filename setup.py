8d33c8a1c61d2639e085819cb58e470f7ffebdee
Pietot <102545552+Pietot@users.noreply.github.com>
2024-09-08 19:33:24 +0200

    Updtaed setup.py

--- setup.py
+++ setup.py
@@ -3,18 +3,24 @@ from setuptools import find_packages, setup
 with open("requirements.txt", encoding="utf-8") as f:
     required = f.read().splitlines()
 
-long_description = "This Mazer Maker Solver made entirely in Python is a program where you can generate maze with many different algorithm and solving them with different pathfinders. Nothing more, nothing less. More info at https://github.com/Pietot/Lapyrinth"
+LONG_DESCRIPTION = "This Mazer Maker Solver made entirely in Python is a program where \
+you can generate maze with many different algorithm and solving them with different pathfinders. \
+Nothing more, nothing less. More info at https://github.com/Pietot/Lapyrinth"
 
 setup(
     name="lapyrinth",
-    version="1.14",
+    version="1.14.1",
     packages=find_packages(),
+    package_data={
+        "": ["*.pyi"],
+    },
     install_requires=required,
     author="Piétôt",
     author_email="baptiste.blasquez@gmail.com",
     maintainer="Piétôt",
-    description="A program capable of creating mazes with many different algorithms and solving them with different pathfinders.",
-    long_description=long_description,
+    description="A program capable of creating mazes with many different \
+algorithms and solving them with different pathfinders.",
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/Pietot/Lapyrinth",
     keywords=["maze", "pathfinding", "algorithm", "labyrinth", "pathfinder", "maze generator"],
